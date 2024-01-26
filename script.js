c6250257186e951897dcbbcc1e80ef664ef9c628
Anderson Dutra <andersonportugaall@gmail.com>
2024-01-26 18:39:23 -0300

    initial commit

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ function calculate(){
     if (imc < 18.5){
         text='Você esta bem magro'
     }else if(imc < 25){
-        text='Você esta com peso normal'
+        text='Você esta com peso normal, Parabéns '
     } else if (imc <29){
-        text='Você esta com sobre peso'
+        text='Cuidado ... Você esta com sobre peso'
     }else if (imc < 39 ){
-        text='Voce esta com Obsesidade'
+        text='Vishiii Maria ... Voce esta com Obsesidade'
     }else if (imc > 39 ){
-        text='Sua situação esta terrivel'
+        text='Sua situação esta terrivel, Tente comer ovo direto'
     }
         
     document.getElementById('resultado').innerText=text
